38a517deba013675d5d23bc88df75dc0be5e24db
Dexrn ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2025-08-26 16:21:10 -0700

    Update PreLoginPacket.java

--- src/main/java/net/lcpr/protocol/packet/c2s/PreLoginPacket.java
+++ src/main/java/net/lcpr/protocol/packet/c2s/PreLoginPacket.java
@@ -1,1 +1,22 @@
+/**
+ * Sent from the client to initiate login
+ *
+ * @c2s Tells the server that the client is ready to login
+ */
+@Getter
+@Setter
+public class PreLoginPacket extends Packet {
+    private short id; // might be something different but the type is correct
+    private String name;
 
+    @Override
+    public void read(EndianInputStream inputStream) throws IOException {
+        id = inputStream.readShort();
+        name = inputStream.readUtf(0x20);
+    }
+
+    @Override
+    public void write(EndianOutputStream outputStream) throws IOException {
+      // todo
+    }
+}

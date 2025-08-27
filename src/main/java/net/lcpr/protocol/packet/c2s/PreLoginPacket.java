1b9a951c8178d5092bb0833422d77a2adb889f5f
Dexrn ZacAttack <dexrn@hotmail.com>
2025-08-26 17:20:53 -0700

    more fixing

--- src/main/java/net/lcpr/protocol/packet/c2s/PreLoginPacket.java
+++ src/main/java/net/lcpr/protocol/packet/c2s/PreLoginPacket.java
@@ -1,9 +1,17 @@
 package net.lcpr.protocol.packet.c2s;
 
+import lombok.Getter;
+import lombok.Setter;
+import net.lcpr.protocol.packet.Packet;
+import net.lcpr.protocol.utils.EndianInputStream;
+import net.lcpr.protocol.utils.EndianOutputStream;
+
+import java.io.IOException;
+
 /**
  * Sent from the client to initiate login
  *
- * @c2s Tells the server that the client is ready to login
+ * @c2s Tells the server that the client is ready to log in
  */
 @Getter
 @Setter
@@ -16,12 +24,12 @@ public class PreLoginPacket extends Packet {
     @Override
     public void read(EndianInputStream inputStream) throws IOException {
         protocolVersion = inputStream.readShort();
-        name = inputStream.readUtf(0x20);
+        name = inputStream.readUTF(0x20);
     }
 
     @Override
     public void write(EndianOutputStream outputStream) throws IOException {
         outputStream.writeShort(PROTOCOL_VERSION); // THIS IS PROTOCOL VER
-        outputStream.writeUtf(this.name);
+        outputStream.writeUTF(this.name);
     }
 }

def08e67b49558abd99a16a1307b28f79e33dc4e
Aurora <auroranova8756@gmail.com>
2025-08-27 19:02:36 +0100

    SetSpawnPositionPacket javadocs

--- src/main/java/net/lcpr/protocol/packet/s2c/SetSpawnPositionPacket.java
+++ src/main/java/net/lcpr/protocol/packet/s2c/SetSpawnPositionPacket.java
@@ -9,9 +9,17 @@ import net.lcpr.protocol.utils.Vec;
 
 import java.io.IOException;
 
+/**
+ * Sets the spawn position of the player
+ *
+ * @s2c Tells the client about it's new spawn position
+ */
 @Getter
 @Setter
 public class SetSpawnPositionPacket extends Packet {
+    /**
+     * The new spawn position for the player
+     */
     private Vec.i3 pos;
 
     @Override

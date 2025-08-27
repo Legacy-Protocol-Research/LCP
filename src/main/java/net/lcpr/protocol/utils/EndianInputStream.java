6e913745c80b3c7cecb09b8240000792df3e016a
Aurora <auroranova8756@gmail.com>
2025-08-27 17:27:14 +0100

    Add stream docs

--- src/main/java/net/lcpr/protocol/utils/EndianInputStream.java
+++ src/main/java/net/lcpr/protocol/utils/EndianInputStream.java
@@ -6,7 +6,7 @@ import net.lcpr.protocol.LegacyConsoleProtocol;
 import java.io.*;
 
 /**
- * An output stream clone which supports the 2 different endian types used in LCE (Little Endian and Big Endian)
+ * An input stream clone which supports the 2 different endian types used in LCE (Little Endian and Big Endian)
  */
 public class EndianInputStream {
     private final LittleEndianDataInputStream little;
@@ -26,88 +26,177 @@ public class EndianInputStream {
         }
     }
 
+    /**
+     * Returns a {@code 0-255} byte data as an {@code int}, or {@code -1} if the stream has ended.
+     *
+     * @return The next byte of data, or {@code -1} if the end of the stream has been reached.
+     * @throws IOException If an I/O error occurs
+     */
     public int read() throws IOException {
         if (isLittleEndian) return little.read();
         else return big.read();
     }
 
+    /**
+     * Reads the stream fully into the byte array provided.
+     *
+     * @param b the byte array to write to
+     * @throws IOException If an I/O error occurs
+     * @throws EOFException If the end of stream is reached before reading fully
+     */
     public void readFully(byte[] b) throws IOException {
-        if (isLittleEndian) little.readFully(b);
-        else big.readFully(b);
+        readFully(b, 0, b.length);
     }
 
+    /**
+     * Reads the stream fully into the byte array provided with an offset and length.
+     *
+     * @param b the byte array to write to
+     * @throws IOException If an I/O error occurs
+     * @throws EOFException If the end of stream is reached before reading fully
+     */
     public void readFully(byte[] b, int off, int len) throws IOException {
         if (isLittleEndian) little.readFully(b, off, len);
         else big.readFully(b, off, len);
     }
 
+    /**
+     * Reads a boolean from the stream
+     *
+     * @return the boolean value
+     * @throws IOException If an I/O error occurs
+     */
     public boolean readBoolean() throws IOException {
         if (isLittleEndian) return little.readBoolean();
         else return big.readBoolean();
     }
 
+    /**
+     * Reads a byte from the stream
+     *
+     * @return the byte value
+     * @throws IOException If an I/O error occurs
+     */
     public byte readByte() throws IOException {
         if (isLittleEndian) return little.readByte();
         else return big.readByte();
     }
 
+    /**
+     * Reads an unsigned byte from the stream
+     *
+     * @return the unsigned byte value as an int
+     * @throws IOException If an I/O error occurs
+     */
     public int readUnsignedByte() throws IOException {
         if (isLittleEndian) return little.readUnsignedByte();
         else return big.readUnsignedByte();
     }
 
+    /**
+     * Reads a short from the stream
+     *
+     * @return the short value
+     * @throws IOException If an I/O error occurs
+     */
     public short readShort() throws IOException {
         if (isLittleEndian) return little.readShort();
         else return big.readShort();
     }
 
+    /**
+     * Reads an unsigned short from the stream
+     *
+     * @return the unsigned short value as an int
+     * @throws IOException If an I/O error occurs
+     */
     public int readUnsignedShort() throws IOException {
         if (isLittleEndian) return little.readUnsignedShort();
         else return big.readUnsignedShort();
     }
 
+    /**
+     * Reads a char from the stream
+     *
+     * @return the char value
+     * @throws IOException If an I/O error occurs
+     */
     public char readChar() throws IOException {
         if (isLittleEndian) return little.readChar();
         else return big.readChar();
     }
 
+    /**
+     * Reads an int from the stream
+     *
+     * @return the int value
+     * @throws IOException If an I/O error occurs
+     */
     public int readInt() throws IOException {
         if (isLittleEndian) return little.readInt();
         else return big.readInt();
     }
 
+    /**
+     * Reads an unsigned int from the stream
+     *
+     * @return the unsigned int value as a long
+     * @throws IOException If an I/O error occurs
+     */
+    public long readUnsignedInt() throws IOException {
+        return (isLittleEndian ? little : big).readInt() & 0xffffffffL;
+    }
+
+    /**
+     * Reads a long from the stream
+     *
+     * @return the long value
+     * @throws IOException If an I/O error occurs
+     */
     public long readLong() throws IOException {
         if (isLittleEndian) return little.readLong();
         else return big.readLong();
     }
 
+    /**
+     * Reads a float from the stream
+     *
+     * @return the float value
+     * @throws IOException If an I/O error occurs
+     */
     public float readFloat() throws IOException {
         if (isLittleEndian) return little.readFloat();
         else return big.readFloat();
     }
 
+    /**
+     * Reads a double from the stream
+     *
+     * @return the double value
+     * @throws IOException If an I/O error occurs
+     */
     public double readDouble() throws IOException {
         if (isLittleEndian) return little.readDouble();
         else return big.readDouble();
     }
 
+    /**
+     * Reads a string from the stream
+     *
+     * @return the string value
+     * @throws IOException If an I/O error occurs
+     */
     public String readUTF(int maxLength) throws IOException {
         StringBuilder builder = new StringBuilder();
 
-        short length = isLittleEndian ? little.readShort() : big.readShort();
+        short length = readShort();
 
         if (length > maxLength) {
             LegacyConsoleProtocol.LOGGER.warn("Received string length longer than maximum allowed ({} > {})", length, maxLength);
         }
 
-        if (isLittleEndian) {
-            for (int i = 0; i < length; ++i) {
-                builder.append(little.readChar());
-            }
-        } else {
-            for (int i = 0; i < length; ++i) {
-                builder.append(big.readChar());
-            }
+        for (int i = 0; i < length; ++i) {
+            builder.append(readChar());
         }
 
         return builder.toString();
--- src/main/java/net/lcpr/protocol/utils/EndianOutputStream.java
+++ src/main/java/net/lcpr/protocol/utils/EndianOutputStream.java
@@ -25,74 +25,138 @@ public class EndianOutputStream {
         }
     }
 
+    /**
+     * Writes a byte to the stream (The lower 8 bits of the int)
+     *
+     * @param v the byte value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void write(int v) throws IOException {
         if (isLittleEndian) little.write(v);
         else big.write(v);
     }
 
+    /**
+     * Writes a boolean to the stream
+     *
+     * @param v the boolean value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeBoolean(boolean v) throws IOException {
         if (isLittleEndian) little.writeBoolean(v);
         else big.writeBoolean(v);
     }
 
+    /**
+     * Writes a byte to the stream
+     *
+     * @param v the byte value (represented as an int) to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeByte(int v) throws IOException {
         if (isLittleEndian) little.writeByte(v);
         else big.writeByte(v);
     }
 
+    /**
+     * Writes a short to the stream
+     *
+     * @param v the short value (represented as an int) to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeShort(int v) throws IOException {
         if (isLittleEndian) little.writeShort(v);
         else big.writeShort(v);
     }
 
+    /**
+     * Writes a char to the stream
+     *
+     * @param v the char value (represented as an int) to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeChar(int v) throws IOException {
         if (isLittleEndian) little.writeChar(v);
         else big.writeChar(v);
     }
 
+    /**
+     * Writes a int to the stream
+     *
+     * @param v the int value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeInt(int v) throws IOException {
         if (isLittleEndian) little.writeInt(v);
         else big.writeInt(v);
     }
 
+    /**
+     * Writes a long to the stream
+     *
+     * @param v the long value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeLong(long v) throws IOException {
         if (isLittleEndian) little.writeLong(v);
         else big.writeLong(v);
     }
 
+    /**
+     * Writes a float to the stream
+     *
+     * @param v the float value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeFloat(float v) throws IOException {
         if (isLittleEndian) little.writeFloat(v);
         else big.writeFloat(v);
     }
 
+    /**
+     * Writes a double to the stream
+     *
+     * @param v the double value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeDouble(double v) throws IOException {
         if (isLittleEndian) little.writeDouble(v);
         else big.writeDouble(v);
     }
 
+    /**
+     * Writes a string as bytes to the stream
+     *
+     * @param s the string value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeBytes(String s) throws IOException {
         if (isLittleEndian) little.writeBytes(s);
         else big.writeBytes(s);
     }
 
+    /**
+     * Writes a string as chars to the stream
+     *
+     * @param s the string value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeChars(String s) throws IOException {
         if (isLittleEndian) little.writeChars(s);
         else big.writeChars(s);
     }
 
+    /**
+     * Writes a string to the stream
+     *
+     * @param s the string value to write
+     * @throws IOException If an I/O error occurs
+     */
     public void writeUTF(String s) throws IOException {
-        if (isLittleEndian) {
-            little.writeShort(s.length());
+        writeShort(s.length());
 
-            for (char character : s.toCharArray()) {
-                little.writeChar(character);
-            }
-        } else {
-            big.writeShort(s.length());
-
-            for (char character : s.toCharArray()) {
-                big.writeChar(character);
-            }
+        for (char character : s.toCharArray()) {
+            writeChar(character);
         }
     }
 }

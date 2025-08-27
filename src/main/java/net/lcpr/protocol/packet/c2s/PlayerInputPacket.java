7bdfb90bca51a2e3edf77aa45c5de6dfb8ae5df2
Aurora <auroranova8756@gmail.com>
2025-08-27 19:04:06 +0100

    Partial PlayerInputPacket javadocs

--- src/main/java/net/lcpr/protocol/packet/c2s/PlayerInputPacket.java
+++ src/main/java/net/lcpr/protocol/packet/c2s/PlayerInputPacket.java
@@ -8,12 +8,23 @@ import net.lcpr.protocol.utils.EndianOutputStream;
 
 import java.io.IOException;
 
+/**
+ * Sends the current input of the player to the server
+ *
+ * @c2s Tells the server about the client's inputs currently
+ */
 @Getter
 @Setter
 public class PlayerInputPacket extends Packet {
     private float xxa; // TODO find out what these are
     private float zza; // TODO find out what these are
+    /**
+     * Whether the player is jumping
+     */
     private boolean isJumping;
+    /**
+     * Whether the player is sneaking
+     */
     private boolean isSneaking;
 
     @Override
